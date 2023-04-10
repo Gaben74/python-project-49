03a13b1a2f0f375e50f5f4d1c365bd300dd65feb
Evgenii Osipian <evgen74111@gmail.com>
2023-04-10 18:59:33 +0300

    2nd Code Review

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -11,7 +11,7 @@ OPERATORS = ((operator.add, '+'),
 def play_game():
     number1 = randint(1, 50)
     number2 = randint(1, 50)
-    operator_type, operator_text = choice(OPERATORS)
+    random_operator, operator_text = choice(OPERATORS)
     question = f"{number1} {operator_text} {number2}"
-    answer = str(operator_type(number1, number2))
+    answer = str(random_operator(number1, number2))
     return answer, question
